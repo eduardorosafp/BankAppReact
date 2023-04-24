bedf9029327d65e8ac82d8817278fbc67d22c682
eduardo rosa <eduardorosafp@gmail.com>
2023-04-24 15:06:35 -0300

    att-website

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import React from 'react'
-import styles from './style'
+import styles from "./style";
+import Navbar from "./components/Navbar";
+import Billing from "./components/Billing";
+import CardDeal from "./components/CardDeal";
+import Business from "./components/Business";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Stats from "./components/Stats";
+import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
+import Hero from "./components/Hero";
 
-const App = () =>  (
+
+const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-   <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-    <div className={`${styles.boxWidth}`}>
-      NavBar
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-   </div>
 
-   <div className={` bg-primary ${styles.flexStart}`}>
-   <div className={`${styles.boxWidth}`}>
-    Hero
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
-   </div>
-
-   <div className={` bg-primary ${styles.paddingX}${styles.flexStart}`}>
-   <div className={`${styles.boxWidth}`}>
-    Stats
-    Bussiness
-    Billing
-    CardDeal
-    Testimonials
-    Clients
-    CTA
-    Footer
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
     </div>
-   </div>
-
-
-
   </div>
-  );
+);
 
-
-export default App
+export default App;
